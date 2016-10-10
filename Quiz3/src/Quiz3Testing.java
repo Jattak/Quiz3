526e26ae17bbfbfc594972d72631d50d28efddcc
Jeffrey <Jeffrey@128.4.170.230>
2016-10-09 23:56:45 -0400

    Quiz3 submission

---
+++ Quiz3/src/Quiz3Testing.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Quiz3Testing {
+
+	@Test
+	public void test() {
+		Triangle getPerimeter = new Triangle();
+		//TODO Not sure how to test
+	}
+
+}
+ 
